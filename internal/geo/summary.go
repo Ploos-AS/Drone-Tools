690c85c0037734d0a19f30449171c09269b394c3
pos <pgousdal@users.noreply.github.com>
2026-09-07 10:51:42 +0200

    M1.2: add shared geospatial summary model

---
+++ internal/geo/summary.go
@@ -0,0 +1,37 @@
+package geo
+
+type Bounds struct {
+	MinLat float64 `json:"min_lat"`
+	MinLon float64 `json:"min_lon"`
+	MaxLat float64 `json:"max_lat"`
+	MaxLon float64 `json:"max_lon"`
+}
+
+type Summary struct {
+	Format       string  `json:"format"`
+	Features     int     `json:"features"`
+	Points       int     `json:"points"`
+	LineStrings  int     `json:"line_strings"`
+	Polygons     int     `json:"polygons"`
+	Coordinates  int     `json:"coordinates"`
+	Bounds       *Bounds `json:"bounds,omitempty"`
+}
+
+type accumulator struct {
+	summary Summary
+	set     bool
+}
+
+func (a *accumulator) addCoord(lon, lat float64) {
+	a.summary.Coordinates++
+	if !a.set {
+		a.summary.Bounds = &Bounds{MinLat: lat, MinLon: lon, MaxLat: lat, MaxLon: lon}
+		a.set = true
+		return
+	}
+	b := a.summary.Bounds
+	if lat < b.MinLat { b.MinLat = lat }
+	if lat > b.MaxLat { b.MaxLat = lat }
+	if lon < b.MinLon { b.MinLon = lon }
+	if lon > b.MaxLon { b.MaxLon = lon }
+}
